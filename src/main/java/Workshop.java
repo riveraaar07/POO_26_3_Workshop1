bcea3a8a7db2c850ab5183ab4ffa7d4369450620
Jorge Rivera <jorgeivanriveraalvarez@gmail.com>
2026-09-06 15:58:58 -0500

    Solucion Encontrar elemento de un arreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -116,8 +116,11 @@ public class Workshop {
 
     // 11 Método que busca un elemento en un arreglo
     public boolean buscarElemento(int[] arreglo, int elemento) {
-        // TODO: Implementar el método para buscar un elemento en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y elemento = 3, el resultado debería ser true.
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == elemento) {
+                return true;
+            }
+        }
         return false;
     }
 
