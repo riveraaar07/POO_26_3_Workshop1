ac47e7226a998ac4f1398b4768c663ea6052fbd9
Jorge Rivera <jorgeivanriveraalvarez@gmail.com>
2026-09-07 12:18:12 -0500

    Solucion ReemplazaryBuscar Cadena

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -253,16 +253,12 @@ public class Workshop {
 
     // 23 Método que reemplaza una subcadena en una cadena por otra subcadena
     public String reemplazarSubcadena(String cadena, String antiguaSubcadena, String nuevaSubcadena) {
-        // TODO: Implementar el método para reemplazar una subcadena en una cadena por otra subcadena.
-        // Ejemplo: Si cadena = "Hello Java", antiguaSubcadena = "Java", y nuevaSubcadena = "world", el resultado debería ser "Hello world".
-        return "";
+        return cadena.replace(antiguaSubcadena, nuevaSubcadena);
     }
 
     // 24 Método que busca una subcadena en una cadena y retorna su índice
     public int buscarSubcadena(String cadena, String subcadena) {
-        // TODO: Implementar el método para buscar una subcadena en una cadena y retornar su índice.
-        // Ejemplo: Si cadena = "Hello world" y subcadena = "world", el resultado debería ser 6.
-        return -1;
+        return cadena.indexOf(subcadena);
     }
 
     // 25 Método que valida un correo electrónico
